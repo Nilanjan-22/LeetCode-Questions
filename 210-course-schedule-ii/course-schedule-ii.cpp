929ae5112939320f3c64792e32a893eb10cf01de
Nilanjan-22 <nilanjande9@gmail.com>
2025-06-06 01:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (52.42%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
+    bool dfs(int i, stack<int>& st, vector<int>& vis,vector<int>& pathVis, vector<vector<int>>& adj){
+        vis[i]=1;
+        pathVis[i]=1;
+        for(auto it: adj[i]){
+            if(vis[it]==0){
+                if(dfs(it,st,vis,pathVis,adj))return true;
+            }
+            else if(pathVis[it]==1&&vis[it]==1)return true;
+        }
+        st.push(i);
+        pathVis[i]=0;
+        return false;
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-
-        vector<int>inDeg(numCourses);
+        stack<int> st;
+        vector<int> vis(numCourses,0);
+        vector<int> pathVis(numCourses,0);
         for(int i=0;i<numCourses;i++){
-            for(auto it: adj[i])inDeg[it]++;
-        }
-
-        queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(inDeg[i]==0)q.push(i);
-        }
-
-        vector<int> topo;
-        while(!q.empty()){
-            int node=q.front();
-            topo.push_back(node);
-            q.pop();
-
-            for(auto it: adj[node]){
-                inDeg[it]--;
-                if(inDeg[it]==0)q.push(it);
+            if(vis[i]==0){
+                if(dfs(i,st,vis,pathVis,adj))return {};
             }
         }
-
-        if(topo.size()!=numCourses)return {};
-        reverse(topo.begin(),topo.end());
+        int n=st.size();
+        vector<int> topo(n);
+        for(int i=n-1;i>=0;i--){
+            topo[i]=st.top();
+            st.pop();
+        }
         return topo;
     }
 };
