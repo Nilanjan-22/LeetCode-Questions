fbc8016b45f518ea325fe902b3246c6d0636466b
Nilanjan-22 <nilanjande9@gmail.com>
2025-06-05 22:21:14 +0530

    Time: 6 ms (55.82%) | Memory: 19.4 MB (65.28%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+
+        vector<int>inDeg(numCourses);
+        for(int i=0;i<numCourses;i++){
+            for(auto it: adj[i])inDeg[it]++;
+        }
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(inDeg[i]==0)q.push(i);
+        }
+
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            topo.push_back(node);
+            q.pop();
+
+            for(auto it: adj[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0)q.push(it);
+            }
+        }
+
+        if(topo.size()==numCourses)return true;
+        else return false;
+
+    }
+};
