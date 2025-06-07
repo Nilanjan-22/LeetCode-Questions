34c7e18253a0fa8249c39793c401970c4a230ebf
Nilanjan-22 <nilanjande9@gmail.com>
2025-06-07 18:03:20 +0530

    Time: 158 ms (12.00%) | Memory: 57.6 MB (9.78%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,26 +4,27 @@ public:
         //simply use dijkstra
         int n=grid.size();
         int m=grid[0].size();
-        priority_queue<vector<int> , vector<vector<int>>, greater<vector<int>>> pq;
+        set<vector<int>> pq;
         vector<int> mi={0,0,-1,1,-1,1,-1,1};
         vector<int> mj={-1,1,0,0,1,1,-1,-1};   // all the possible 8 movements
 
         if(grid[0][0]==1)return -1;
 
-        pq.push({1,0,0});
+        pq.insert({1,0,0});
         vector<vector<int>> dist(n,vector<int>(m,1e9));
         dist[0][0]=1;
         while(!pq.empty()){
-            int d=pq.top()[0];
-            int i=pq.top()[1];
-            int j=pq.top()[2];
-            pq.pop();
+            int d=(*pq.begin())[0];
+            int i=(*pq.begin())[1];
+            int j=(*pq.begin())[2];
+            pq.erase({d,i,j});
             for(int it=0;it<8;it++){
                 int i1=i+mi[it], j1=j+mj[it];
                 if(i1<n && i1>-1 && j1<m && j1>-1 && grid[i1][j1]==0){
                     if(d+1<dist[i1][j1]){
+                        if(dist[i1][j1]!=1e9)pq.erase({dist[i1][j1],i1,j1});
                         dist[i1][j1]=d+1;
-                        pq.push({d+1,i1,j1});
+                        pq.insert({d+1,i1,j1});
                     }
                 }
             }
