010622d3f7588dd2584fa9a7dff732cc90bdead4
Nilanjan-22 <nilanjande9@gmail.com>
2025-06-11 14:30:35 +0530

    Time: 7 ms (62.30%) | Memory: 14.5 MB (34.00%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,37 +1,96 @@
+class DisjointSet {
+    
+public:
+    vector<int> rank,size,parent;
+    DisjointSet(int n) {
+     rank.resize(n+1,0);
+     size.resize(n+1,1);
+     parent.resize(n+1);
+     for(int i=0;i<n+1;i++){
+        parent[i]=i;
+     }
+    }
+    int U_par(int n){
+        if(parent[n]==n)return n;
+        int p=parent[n];
+        return parent[n]= U_par(p);
+    }
+
+    bool find(int u, int v) {
+        if(U_par(u)==U_par(v))return true;
+        return false;
+    }
+
+    void unionByRank(int u, int v) {
+        int upu=U_par(u);
+        int upv=U_par(v);
+
+        if(upu==upv)return;
+        if(rank[upu]<rank[upv]){
+            parent[upu]=upv;
+        }
+        else if(rank[upv]<rank[upu]){
+            parent[upv]=upu;
+        }
+        else {
+            parent[upv]=upu;
+            rank[upu]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int upu=U_par(u);
+        int upv=U_par(v);
+
+        if(upu==upv)return;
+        if(size[upu]<size[upv]){
+            parent[upu]=upv;
+            size[upv]+=size[upu];
+        }
+        else {
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+    }
+};
 class Solution {
 public:
-    vector<int> x={0,0,-1,1};
-    vector<int> y={-1,1,0,0};
-    bool dfs(int i, int j, int max, vector<vector<int>>& vis, vector<vector<int>>& grid){
-        int n=grid.size();
-        if(i>=n || j>=n || i<0 || j<0 || max<grid[i][j] || vis[i][j]==1)return false;
-        if(i==n-1 && j==n-1 && max>=grid[i][j])return true;
-        vis[i][j]=1;
-        bool ans=false;
-        for(int it=0;it<4;it++){
-            int x1=i+x[it], y1=j+y[it];
-            ans|=dfs(x1,y1,max,vis,grid);
-            if(ans==true)return ans;
-        }
-        return ans;
-    }
     int swimInWater(vector<vector<int>>& grid) {
-        // use dfs and binary search on ans
-        int low=0,high=2500;
-        int ans=0;
         int n=grid.size();
-        while(low<=high){
-            int mid=(low+high)/2;
-            vector<vector<int>> vis(n,vector<int>(n,0));
-            if(dfs(0,0,mid,vis,grid)){
-                ans=mid;
-                high=mid-1;
+        DisjointSet ds(n*n);
+
+        vector<vector<int>> vis(n,vector<int>(n,0));
+        vector<int> x={-1,1,0,0};
+        vector<int> y={0,0,1,-1};
+        vector<pair<int,pair<int,int>>> sqs;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sqs.push_back({grid[i][j],{i,j}});
             }
-            else{
-                low=mid+1;
-            }
-            vis.clear();
         }
-        return ans;
+        sort(sqs.begin(),sqs.end());
+        for(auto sq : sqs){
+            int h=sq.first;
+            int i=sq.second.first;
+            int j=sq.second.second;
+
+            vis[i][j]=1;
+            int p=i*n+j;
+
+            for(int it=0;it<4;it++){
+                int nx=i+x[it],ny=j+y[it];
+
+                if(nx>=n || nx<0 || ny<0 || ny>=n || vis[nx][ny]==0)continue;
+                else{
+                    ds.unionBySize(p, nx*n+ny);
+                }
+            }
+            
+            if(ds.U_par(0)== ds.U_par(n*n-1))return h;
+        }
+
+        return -1;
+        
     }
 };
