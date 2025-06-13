1128361e0f8f88db8563f5627f00ed07fb56e4c3
Nilanjan-22 <nilanjande9@gmail.com>
2025-06-13 21:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.85%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,42 +11,14 @@
  */
 class Solution {
 public:
-    // do bfs traversal of both together if mismatch return false
-    bool bfs(TreeNode* node1, TreeNode* node2){
-        queue<TreeNode*> q1,q2;
-        if(node1==NULL && node2==NULL)return true;
-        if(node1==NULL || node2==NULL || node1->val !=node2->val)return false;
-        q1.push(node1);
-        q2.push(node2);
-        while(!q1.empty()){
-            int s=q1.size();
-            for(int i=0;i<s;i++){
-                TreeNode* n1=q1.front();
-                TreeNode* n2=q2.front();
-                if((n1->left==NULL && n2->left!=NULL)  || (n2->left==NULL && n1->left!=NULL))return false;
-                if((n1->right==NULL && n2->right!=NULL)  || (n2->right==NULL && n1->right!=NULL))return false;
-                if(n1->left==NULL && n2->left==NULL && n1->right==NULL && n2->right==NULL){
-                    q1.pop();
-                    q2.pop();
-                    continue;
-                };
-                if((n1->left!=NULL && n2->left!=NULL && n1->left->val != n2->left->val) || (n1->right!=NULL && n2->right!=NULL && n1->right->val !=n2->right->val))return false;
-
-                if(n1->left !=NULL){
-                    q1.push(n1->left);
-                    q2.push(n2->left);
-                }
-                if(n1->right !=NULL){
-                    q1.push(n1->right);
-                    q2.push(n2->right);
-                }
-                q1.pop();
-                q2.pop();
-            }
-        }
-        return true;
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return bfs(p,q);
+        if(p==NULL && q==NULL)return true;
+
+        if(p==NULL || q==NULL)return false;
+
+        if(p->val == q->val){
+            return isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
+        }
+        return false;
     }
 };
