46c74c2f35c68f6f50321d031fa9999875d036d0
Nilanjan-22 <nilanjande9@gmail.com>
2025-06-15 01:55:15 +0530

    Time: 4 ms (1.71%) | Memory: 8.3 MB (18.49%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        int cp=num;
+        vector<int> digits;
+        while(cp>0){
+            digits.push_back(cp%10);
+            cp/=10;
+        }
+        reverse(digits.begin(),digits.end());
+        vector<int> dig=digits;
+        int chng=-1;
+        int n=digits.size();
+        for(int i=0;i<n;i++){
+            if(dig[i]>0 && chng==-1)chng=dig[i];
+            if(dig[i]==chng)dig[i]=0;
+        }
+        for(int i=0;i<dig.size();i++)cout<<dig[i]<<" ";
+        chng=-1;
+        for(int i=0;i<n;i++){
+            if(digits[i]<9 && chng==-1)chng=digits[i];
+            if(digits[i]==chng)digits[i]=9;
+        }
+
+        int mul=1;
+        int num1=0,num2=0;
+        for(int i=n-1;i>=0;i--){
+            num1+=dig[i]*mul;
+            num2+=digits[i]*mul;
+            mul*=10;
+        }
+        return abs(num1-num2);
+    }
+};
