b23a99af1ead83bf2db72b601d4d9be9f3f9bf12
Nilanjan-22 <nilanjande9@gmail.com>
2025-07-05 01:21:57 +0530

    Time: 2252 ms (5.23%) | Memory: 418.1 MB (5.23%) - LeetSync

---
+++ 2027-maximum-number-of-removable-characters/maximum-number-of-removable-characters.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool pos(string& s, string& p, vector<int>& removable, int k){
+        if(k==0)return true;
+
+        set<int> v;
+        for(int i=0;i<k;i++){
+            v.insert(removable[i]);
+        }
+
+        int i=0,j=0;
+        while(i<s.size() && j<p.size()){
+            if(v.find(i)!=v.end())i++;
+            else if(s[i]==p[j]){
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+
+        if(j==p.size())return true;
+        return false;
+    }
+    int maximumRemovals(string s, string p, vector<int>& removable) {
+        // use binary search
+        int n=s.size(),m=p.size(),r=removable.size();
+        int low=0,high=r;
+        int ans=0;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(pos(s,p,removable,mid)){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+
+        return ans;
+    }
+};
