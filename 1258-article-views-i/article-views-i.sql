35d23511fec8637ae295d6354b4f8626f0509faf
Nilanjan-22 <nilanjande9@gmail.com>
2025-08-17 19:34:47 +0530

    Time: 921 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1258-article-views-i/article-views-i.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+-- ALTER TABLE Views RENAME COLUMN author_id TO id;
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id=viewer_id ORDER BY author_id ASC;
