aa428eb5cae0c61413d3fc80e488dfc3db4ecf9e
Nilanjan-22 <nilanjande9@gmail.com>
2025-08-30 00:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (12.44%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     long long flowerGame(int n, int m) {
         long long ans=0;
-
-        for(int i=1;i<=n;i++){
-            if(i%2==0)ans+=(m+1)/2;
-            else ans+=m/2;
-        }
+        long long ev=n/2, od=(n+1)/2;
+        ans+= ev*(long long)((m+1)/2);
+        ans+=od*(long long)(m/2);
+        // for(int i=1;i<=n;i++){
+        //     if(i%2==0)ans+=(m+1)/2;
+        //     else ans+=m/2;
+        // }
         return ans;
     }
 };
