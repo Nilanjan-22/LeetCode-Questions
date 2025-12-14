a03627ed93692783aeec64b64fe2e1869fd094cc
Nilanjan-22 <nilanjande9@gmail.com>
2025-12-15 01:14:07 +0530

    Time: 18 ms (84.34%) | Memory: 58.6 MB (52.02%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        long long cnt=0,ans=0,m=1e9+7;
+        vector<int> points;
+        for(int i=0; i<corridor.size();i++){
+            if(corridor[i]=='S'){
+                cnt++;
+                if(cnt>1)points.push_back(i);
+            }
+            
+        }
+        if(points.size()>1)points.pop_back();
+        if(cnt%2==1 || cnt==0)return ans;
+        else{
+            ans=1;
+            cout<<points.size()<<" ";
+            if(points.size()<2)return ans;
+            for(int j=0;j<points.size()-1;){
+                long long mul=(points[j+1]-points[j]);
+                ans%=m;
+                ans*=mul;
+                ans%=m;
+                j+=2;
+            }
+            return ans;
+        }
+    }
+};
