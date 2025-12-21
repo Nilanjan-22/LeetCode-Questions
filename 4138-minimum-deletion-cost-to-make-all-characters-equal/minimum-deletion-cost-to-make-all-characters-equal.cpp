f84b5a32ea451df7aaf0be7b77dad56cae6609ca
Nilanjan-22 <nilanjande9@gmail.com>
2025-12-21 21:53:25 +0530

    Time: 27 ms (15.38%) | Memory: 115.1 MB (7.69%) - LeetSync

---
+++ 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long minCost(string s, vector<int>& cost) {
+        map<char,long long> mp;
+        long long total=0;
+        for(int i=0;i<s.size();i++){
+            if(mp.find(s[i])==mp.end())mp[s[i]]=cost[i];
+            else mp[s[i]]+=cost[i];
+            total+=cost[i];
+        }
+
+        long long maxi=LLONG_MIN;
+
+        for(auto it: mp){
+            maxi=max(maxi,it.second);
+        }
+        return total-maxi;
+
+    }
+};
