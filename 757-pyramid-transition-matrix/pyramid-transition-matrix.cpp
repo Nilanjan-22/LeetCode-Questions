52bca0d51a14839095f68b6f8c4d8c6861118120
Nilanjan-22 <nilanjande9@gmail.com>
2025-12-30 00:49:05 +0530

    Time: 1056 ms (30.27%) | Memory: 168.1 MB (5.41%) - LeetSync

--- 757-pyramid-transition-matrix/pyramid-transition-matrix.cpp
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    bool ans(string& prev, int i , string& cur , map<pair<char,char>, vector<char>>& mp, set<string>& dp){
-        if(prev.size()==1)return true;
+    unordered_map <string, bool> dp;
+    bool ans(string bottom , map<pair<char,char> , vector<char>>& mp , string cur , int i){
+        if(bottom.size()==1)return true;
 
-        if(i==prev.size()-1){
-            if(dp.find(cur)!=dp.end())return false;
-            bool ok=ans(cur,0,*(new string()),mp,dp);
-            if(ok)return true;
-            else {
-                dp.insert(cur);
-                return false;
-            };
+        string key= bottom + "_" + to_string(i) + "_" + cur;
+
+        if(dp.find(key)!=dp.end())return dp[key];
+
+        if(i==bottom.size()-1){
+            return dp[key] = ans(cur,mp,"",0);
         }
 
-        if(mp.find({prev[i],prev[i+1]})==mp.end())return false;
-        vector<char> it= mp[{prev[i],prev[i+1]}];
-        for(auto c: it){
-            cur.push_back(c);
-            if(ans(prev,i+1,cur,mp,dp))return true;
+        if(mp.find({bottom[i], bottom[i+1]})==mp.end()) return dp[key]=false;
+
+        vector<char> cases=mp[{bottom[i], bottom[i+1]}];
+
+        for(auto it: cases){
+            cur.push_back(it);
+            if(ans(bottom,mp,cur,i+1))return dp[key]=true;
             cur.pop_back();
         }
-        return false;
+        return dp[key]=false;
     }
     bool pyramidTransition(string bottom, vector<string>& allowed) {
-        string prev=bottom, cur="";
         map<pair<char,char> , vector<char>> mp;
-        set<string> dp;
         for(int i=0;i<allowed.size();i++){
-            mp[{allowed[i][0], allowed[i][1]}].push_back(allowed[i][2]);
+            mp[{allowed[i][0],allowed[i][1]}].push_back(allowed[i][2]);
         }
 
-        return ans(prev,0,cur,mp,dp);
-
+        return ans(bottom, mp, "", 0);
     }
 };
