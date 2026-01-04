5eb706364f4df76fa58813132f9ad5bdebec7eb1
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-04 16:19:40 +0530

    Time: 8 ms (82.76%) | Memory: 16.2 MB (48.28%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,34 +1,24 @@
 class Solution {
+private:
+    int factors(int n) {
+        int sum = 0, c = 0;
+        for(int i = 2; i*i <= n; i++) {
+            if(n % i == 0) {
+                int j = n / i;
+                if(j == i || c > 0) return 0;
+                sum += i + j;
+                c++;
+            }
+        }
+        if(c == 0) return 0;
+        return 1 + sum + n;
+    }
 public:
     int sumFourDivisors(vector<int>& nums) {
-        vector<bool> is_prime(1e5+1,true);
-        for(int i=2; i*i<=1e5;i++){
-            if(is_prime[i]){
-                for(int j=i*i;j<=1e5;j+=i){
-                    is_prime[j]=false;
-                }
-            }
-        }
-        set<int> prime;
-        for(int i=2;i<1e5+1;i++){
-            if(is_prime[i])prime.insert(i);
-        }
-        // return prime.size();
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            if(prime.find(nums[i])!=prime.end()) continue;
-            else{
-                for(auto it: prime){
-                    if(nums[i]%it==0 && ((prime.find(nums[i]/it)!=prime.end()&& (nums[i]/it)!=it)||(nums[i]/it == pow(it,2)))){
-                        sum+=nums[i]+1;
-                        sum+=it;
-                        sum+=nums[i]/it;
-                        break;
-                    }
-                }
-            }
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += factors(nums[i]);
         }
         return sum;
-
     }
 };
