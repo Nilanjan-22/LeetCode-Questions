2b87862772e599d9fb310fb74dfaf30b01d0d78d
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-08 03:00:58 +0530

    Time: 599 ms (23.84%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-select round(count(case when datediff(event_date, (select min(event_date) from Activity as a1 where a1.player_id=A.player_id group by player_id))=1 then 1 else null end)/count(distinct player_id),2) as'fraction' from Activity as A ;
+WITH ranked_logins AS (
+    SELECT
+        player_id,
+        event_date,
+        MIN(event_date) OVER (PARTITION BY player_id) AS first_login
+    FROM Activity
+)
+select round(count(case when datediff(event_date, first_login)=1 then 1 else null end)/count(distinct player_id),2) as'fraction' from ranked_logins ;
