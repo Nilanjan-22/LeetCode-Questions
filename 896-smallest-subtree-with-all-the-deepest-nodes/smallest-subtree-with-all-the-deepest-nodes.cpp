0831e47ba4b2fbb5b10e7d8702884f52703645ac
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-10 02:07:21 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (52.72%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,28 +11,20 @@
  */
 class Solution {
 public:
-    map<TreeNode* , pair<int,TreeNode*>> mp;
-    int maxd=INT_MIN;
-    void dfs(int dist, TreeNode* parent, TreeNode* node){
-        if(node==NULL)return;
-        maxd=max(dist,maxd);
-        dfs(dist+1,node,node->left);
-        mp[node]={dist,parent};
-        dfs(dist+1,node,node->right);
+    
+    pair<int,TreeNode*> dfs(TreeNode* node){
+        if(node==NULL)return {0,NULL};
+        pair<int,TreeNode*> left= dfs(node->left);
+        pair<int,TreeNode*> right= dfs(node->right);
+
+        if(left.first>right.first) return {left.first+1,left.second};
+        else if(right.first>left.first) return {right.first+1,right.second};
+        else{
+            return {left.first+1,node};
+        }
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        dfs(0,NULL,root);
-        set<TreeNode*> temp;
-        for(auto it:mp){
-            if(it.second.first==maxd)temp.insert(it.first);
-        }
-        while(temp.size()>1){
-            set<TreeNode*> temp2;
-            for(auto it: temp){
-                temp2.insert(mp[it].second);
-            }
-            temp=temp2;
-        } 
-        return *temp.begin();    
+        return dfs(root).second;
+        
     }
 };
