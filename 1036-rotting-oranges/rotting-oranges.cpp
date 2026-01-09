10180e0646b91cb13f8d9338627329403ec089dd
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-10 03:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (7.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
-    int bfs(int n, int c, int m ,queue<tuple<int,int,int>>& q, vector<vector<int>>& g){
-        int ans=0,cnt=0;
+    int n,m,cnt=0;
+    queue<vector<int>> q;
+    vector<vector<int>> g;
+    int bfs(){
+        int ans=0,c=0;
         while(!q.empty()){
-            int i=get<0>(q.front());
-            int j = get<1>(q.front());
-            int t=get<2>(q.front());
-            ans=max(ans,t);
-            cnt++;
+            int i=q.front()[0];
+            int j=q.front()[1];
+            int d=q.front()[2];
+
+            ans=max(d,ans);
             q.pop();
-            int n1,n2;
-            //left
-            n1=i,n2=j-1;
-            if(n1>=0 && n1<n && n2>=0 && n2<m && g[n1][n2]==1){
-                g[n1][n2]=2;
-                q.push({n1,n2,t+1});
-            }
-            //right
-            n1=i,n2=j+1;
-            if(n1>=0 && n1<n && n2>=0 && n2<m && g[n1][n2]==1){
-                g[n1][n2]=2;
-                q.push({n1,n2,t+1});
-            }
             //up
-            n1=i-1,n2=j;
-            if(n1>=0 && n1<n && n2>=0 && n2<m && g[n1][n2]==1){
-                g[n1][n2]=2;
-                q.push({n1,n2,t+1});
+            if(i-1>-1 && g[i-1][j]==1){
+                c++;
+                g[i-1][j]=2;
+                q.push({i-1,j,d+1});
             }
             //down
-            n1=i+1,n2=j;
-            if(n1>=0 && n1<n && n2>=0 && n2<m && g[n1][n2]==1){
-                g[n1][n2]=2;
-                q.push({n1,n2,t+1});
+            if(i+1<n && g[i+1][j]==1){
+                c++;
+                g[i+1][j]=2;
+                q.push({i+1,j,d+1});
             }
+
+            //left
+            if(j-1>-1 && g[i][j-1]==1){
+                c++;
+                g[i][j-1]=2;
+                q.push({i,j-1,d+1});
+            }
+            //right
+            if(j+1<m && g[i][j+1]==1){
+                c++;
+                g[i][j+1]=2;
+                q.push({i,j+1,d+1});
+            }
+            
         }
-        if(cnt==c)return ans;
-        return -1;
+
+        if(c==cnt)return ans;
+        else return -1;
     }
     int orangesRotting(vector<vector<int>>& grid) {
-        
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> gCopy=grid;
-        queue<tuple<int,int,int>> q;
-        int cnt=0;
+        n=grid.size();
+        m=grid[0].size();
+        g=grid;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j,0});
-                    cnt++;
-                }
-                else if(grid[i][j]==1)cnt++;
+                if(g[i][j]==2)q.push({i,j,0});
+                else if(g[i][j]==1)cnt++;
             }
         }
-        
-        return bfs(n,cnt,m,q,gCopy);
 
-        
+        return bfs();
+
     }
 };
