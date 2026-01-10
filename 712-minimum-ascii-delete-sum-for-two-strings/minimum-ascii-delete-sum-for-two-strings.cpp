330a90bedd81516747c97dc3d66276c281e50ded
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-10 14:41:31 +0530

    Time: 38 ms (22.84%) | Memory: 18.2 MB (67.29%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int ans(int i1, int i2, string& s1, string& s2){
+        if(i1>=s1.size() && i2>=s2.size())return 0;
+
+        if(dp[i1][i2]!=-1)return dp[i1][i2];
+        if(i1==s1.size()){
+            return dp[i1][i2]=(int)s2[i2]+ans(i1,i2+1,s1,s2);;
+        }
+        else if(i2==s2.size()){
+            return dp[i1][i2]=(int)s1[i1]+ans(i1+1,i2,s1,s2);;
+        }
+        else if(s1[i1]==s2[i2]){
+            int skip=ans(i1+1,i2+1,s1,s2);
+            int del1=(int)s1[i1]+ans(i1+1,i2,s1,s2);
+            int del2=(int)s2[i2]+ans(i1,i2+1,s1,s2);
+
+            return dp[i1][i2]=min(skip,min(del1,del2));
+        }
+        else{
+            int del1=(int)s1[i1]+ans(i1+1,i2,s1,s2);
+            int del2=(int)s2[i2]+ans(i1,i2+1,s1,s2);
+            return dp[i1][i2]=min(del1,del2);
+        }
+        return 0;
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return ans(0,0,s1,s2);
+    }
+};
