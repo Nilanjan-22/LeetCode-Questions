fbba4a6d3f097c6c2de2611c3aa1c1dea68d7f6a
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-10 14:46:44 +0530

    Time: 38 ms (22.84%) | Memory: 18.5 MB (9.87%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -3,15 +3,24 @@ public:
     vector<vector<int>> dp;
     int ans(int i1, int i2, string& s1, string& s2){
         if(i1>=s1.size() && i2>=s2.size())return 0;
+        if(i1==s1.size()){
+            int val=0;
+            while(i2<s2.size()){
+                val+=(int)s2[i2++];
+            }
+            return val;
+        }
+        if(i2==s2.size()){
+            int val=0;
+            while(i1<s1.size()){
+                val+=(int)s1[i1++];
+            }
+            return val;
+        }
 
         if(dp[i1][i2]!=-1)return dp[i1][i2];
-        if(i1==s1.size()){
-            return dp[i1][i2]=(int)s2[i2]+ans(i1,i2+1,s1,s2);
-        }
-        else if(i2==s2.size()){
-            return dp[i1][i2]=(int)s1[i1]+ans(i1+1,i2,s1,s2);
-        }
-        else if(s1[i1]==s2[i2]){
+        
+        if(s1[i1]==s2[i2]){
             int skip=ans(i1+1,i2+1,s1,s2);
             int del1=(int)s1[i1]+ans(i1+1,i2,s1,s2);
             int del2=(int)s2[i2]+ans(i1,i2+1,s1,s2);
