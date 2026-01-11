5584b001d0668b9720ecc2f5e092bd8e735ea354
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-11 23:10:07 +0530

    Time: 11 ms (33.61%) | Memory: 20.7 MB (25.29%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-    // stack solution
-    int largestRectangleArea(vector<int>& heights) {
-        stack <int> st;
-        int maxArea=0;
-        int n = heights.size();
-        for(int i=0; i<heights.size();i++){
-            if(st.empty() || heights[st.top()]<=heights[i])st.push(i);
-            else{
-                int area;
-                while(!st.empty() && heights[st.top()]>heights[i]){
-                    int h = heights[st.top()];
-                    st.pop();
-                    int l;
-                    if(st.empty())l=-1;
-                    else l=st.top();
+    int ans(vector<int>& heights){
+        int n=heights.size();
+        vector<int> nse(n),pse(n);
+        stack<int> st={};
 
-                    area = h*(i-l-1);
-                    maxArea=max(area,maxArea);
-                }
-                st.push(i);
-            }
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i])st.pop();
+            if(st.empty())pse[i]=-1;
+            else pse[i]=st.top();
+            st.push(i);
         }
-        while(!st.empty()){
-            int h = heights[st.top()];
-            st.pop();
-            int l;
-            if(!st.empty())l=st.top();
-            else l=-1;
-            maxArea = max(maxArea, h*(n-l-1));
+
+        st={};
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i])st.pop();
+            if(st.empty())nse[i]=n;
+            else nse[i]=st.top();
+            st.push(i);
         }
-        return maxArea;
+
+        int maxarea=0;
+        for(int i=0;i<n;i++){
+            maxarea=max(maxarea, (nse[i]-pse[i]-1)*heights[i]);
+        }
+        return maxarea;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n1=matrix.size();
-        int n2=matrix[0].size();
-        vector<int> prefix(n2,0);
-        int ans=0;
-        for(int i=0; i<matrix.size();i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if(matrix[i][j]=='0')prefix[j]=0;
-                else prefix[j]++;
+        vector<int> h(matrix[0].size(),0);
+        int maxarea=-1;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]=='0')h[j]=0;
+                else h[j]++;
             }
-            ans=max(ans,largestRectangleArea(prefix));
+            maxarea=max(maxarea,ans(h));
         }
-        return ans;
+        return maxarea;
     }
 };
