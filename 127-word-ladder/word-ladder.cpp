d944cd029aee5669348b5492011b41cd142e02e6
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-12 13:34:12 +0530

    Time: 356 ms (22.88%) | Memory: 21.3 MB (35.09%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    int ladderLength(string bw, string ew, vector<string>& wl) {
-        //here bw is my src node
-        // we will simply apply bfs in every direction
-        // but before that instead of a visited vector we will use a set
-        //the set will store the strings
-
-        set<string> st;
-        st.insert(bw);
-        int n=wl.size();
-        for(int i=0;i<n;i++){
-            st.insert(wl[i]);
-        }
-
-        //now we will declare the queue which will store pairs
-        //one of which is the string and the other is the number of paths taken to reach that string
-
-        queue<pair<string,int>> q;
-        //we will push the src string
-        q.push({bw,1});
-        st.erase(bw);
+    queue<pair<string,int>> q;
+    set<string> st;
+    int bfs(string endWord){
+        int ans=0;
         while(!q.empty()){
             string s=q.front().first;
             int d=q.front().second;
             q.pop();
-            //we have to go to every possibility and check if it exists in the set
-            //if it does push it in the queue
+            ans=max(ans,d);
+            if(s==endWord)return ans;
+
             for(int i=0;i<s.size();i++){
-                string copy=s;
-                for(char it ='a';it<='z';it++){
-                    copy[i]=it;
-                    if(st.count(copy)>0){
-                        if(copy==ew)return d+1;
-                        q.push({copy,d+1});
-                        st.erase(copy);
+                string c=s;
+                for(char j='a';j<='z';j++){
+                    c[i]=j;
+                    if(st.find(c)!=st.end()){
+                        q.push({c,d+1});
+                        st.erase(c);
                     }
                 }
             }
-
         }
         return 0;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        for(int i=0;i<wordList.size();i++){
+            st.insert(wordList[i]);
+        }
+        q.push({beginWord,1});
+        return bfs(endWord);
+
     }
 };
