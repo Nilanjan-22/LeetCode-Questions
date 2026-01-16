0295a3b06b0035ab2d8aa870c628a5f65d784cef
Nilanjan-22 <nilanjande9@gmail.com>
2026-01-16 12:33:16 +0530

    Time: 808 ms (13.82%) | Memory: 186.1 MB (6.56%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minimumTime(vector<vector<int>>& grid) {
+        if  (grid[0][1] > 1 && grid[1][0] > 1) return -1;
+
         int n=grid.size(),m=grid[0].size();
         priority_queue<vector<int> , vector<vector<int>>, greater<vector<int>>> pq;
         pq.push({0,0,0});
@@ -15,22 +17,22 @@ public:
             if(i==n-1 && j==m-1)return time;
             pq.pop();
 
-            bool loop=false;
-            for(auto it: dir){
-                int ni=i+it[0];
-                int nj=j+it[1];
-                if(ni>=0 && nj>=0 && ni<n && nj<m){
-                    if(grid[ni][nj]<=time+1){
-                        loop=true;
-                        break;
-                    }
-                }
-            }
+            // bool loop=true;
+            // for(auto it: dir){
+            //     int ni=i+it[0];
+            //     int nj=j+it[1];
+            //     if(ni>=0 && nj>=0 && ni<n && nj<m){
+            //         if(grid[ni][nj]<=time+1){
+            //             loop=true;
+            //             break;
+            //         }
+            //     }
+            // }
 
             for(auto it: dir){
                 int ni=i+it[0], nj=j+it[1];
-                if(ni>=0 && nj>=0 && ni<n && nj<m){
-                    if(!loop && grid[ni][nj]>time+1)continue;
+                if(ni>=0 && nj>=0 && ni<n && nj<m && dist[ni][nj]==1e9){
+                    // if(!loop && grid[ni][nj]>time+1)continue;
                     
                     int need;
                     int dif=(grid[ni][nj]-time)/2;
