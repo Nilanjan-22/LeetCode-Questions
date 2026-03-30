3616d2cc57bf12a2206b7e426a7f9ccab4e03013
Nilanjan-22 <nilanjande9@gmail.com>
2026-03-30 12:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (87.36%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        unordered_map<char,int> freq1,freq2;
+        vector<int> freq1(26,0),freq2(26,0);
         int n=s1.size();
         for(int i=0;i<n;i+=2){
-            freq1[s1[i]]++;
-            freq1[s2[i]]--;
+            freq1[s1[i]-'a']++;
+            freq1[s2[i]-'a']--;
         }
         for(int i=1;i<n;i+=2){
-            freq2[s1[i]]++;
-            freq2[s2[i]]--;
+            freq2[s1[i]-'a']++;
+            freq2[s2[i]-'a']--;
         }
 
-        for(auto it: freq1){
-            if(it.second!=0)return false;
-        }
-
-        for(auto it: freq2){
-            if(it.second!=0)return false;
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=0 || freq2[i]!=0)return false;
         }
         
         return true;
