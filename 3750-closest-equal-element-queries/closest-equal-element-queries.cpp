172fb97cc1693f410312ac2d2c762a57806da049
Nilanjan-22 <nilanjande9@gmail.com>
2026-04-16 14:21:37 +0530

    Time: 1495 ms (5.47%) | Memory: 216.7 MB (49.25%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        map<int,vector<int>> mp;
+        int s=nums.size();
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        int n=queries.size();
+        vector<int> ans(n,-1);
+        vector<int> temp;
+        for(int i=0;i<n;i++){
+            int num=nums[queries[i]];
+            temp=mp[num];
+            int size=temp.size();
+            if(size==1)continue;
+
+            int ind = lower_bound(temp.begin(),temp.end(),queries[i])-temp.begin();
+            int mindist=1e9;
+            if(ind!=0){
+                mindist = min(s+temp[0]-queries[i] , queries[i]-temp[ind-1]);
+            }
+            if(ind!=size-1){
+                mindist= min(mindist , temp[ind+1] - queries[i]);
+                mindist = min(mindist ,s+queries[i]-temp[size-1]);
+            }
+
+            ans[i]=mindist;
+        }
+        return ans;
+    }
+};
