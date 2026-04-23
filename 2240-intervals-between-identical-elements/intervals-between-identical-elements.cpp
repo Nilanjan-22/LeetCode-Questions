e3a2be012fc3aa13b981d817defc67f42440efef
Nilanjan-22 <nilanjande9@gmail.com>
2026-04-23 13:21:50 +0530

    Time: 113 ms (75.97%) | Memory: 135.1 MB (72.08%) - LeetSync

---
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& nums) {
+        unordered_map<int, vector<int>> posOfEachNum;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            posOfEachNum[nums[i]].push_back(i);
+        }
+
+        vector<long long> distanceArray(n);
+
+        vector<int> curNumArray;
+        for(auto it: posOfEachNum){
+            curNumArray = it.second;
+            long long dist=0;
+            int len = curNumArray.size();
+            for(int i=1;i<len;i++){
+                dist+=(long long)curNumArray[i]-(long long)curNumArray[0];
+            }
+            distanceArray[curNumArray[0]]=dist;
+            long long change, leftCnt,rightCnt;
+            for(int i=1;i<len;i++){
+                change=curNumArray[i]-curNumArray[i-1];
+                leftCnt=i;
+                rightCnt = len-i;
+                dist+=(leftCnt*change);
+                dist-=(rightCnt*change);
+                distanceArray[curNumArray[i]]=dist;
+            }
+        }
+
+        return distanceArray;
+    }
+};
