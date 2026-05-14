59227e5740e27cad1ca9f484e69f1ed0ade8c023
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-14 18:51:08 +0530

    Time: 7 ms (5.43%) | Memory: 27.3 MB (23.51%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int> mp;
+        int maxi=-1;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            maxi=max(maxi,nums[i]);
+        }
+        if(mp[maxi]!=2)return false;
+        if(n!=maxi+1)return false;
+        else{
+            for(auto it: mp){
+                if(it.second!=1 && it.first!=maxi)return false;
+            }
+        }
+        return true;
+    }
+};
