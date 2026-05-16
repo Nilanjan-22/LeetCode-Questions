6f5940677a947b797275200932c63a23f89d17d5
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-16 11:49:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,7 +9,14 @@ public:
 
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(0,n,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            if(i>1){
+                dp[i]+=dp[i-2];
+            }
+            dp[i]+=dp[i-1];
+        }
+        return dp[n];
     }
 };
