3ed0ab5b2d36fc507578fe8d59905e5f26d7dfd8
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-23 12:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (18.08%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int mismatch=0,mismatchInd=-1,maxi=-1;
+        int mismatch=0;
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]>nums[i+1]){
                 mismatch++;
-                mismatchInd=i;
             }
             if(mismatch>1)return false;
-            maxi=max(maxi,nums[i]);
         }
 
-        if(mismatch==1 && (nums[mismatchInd]!=maxi || nums[nums.size()-1]>nums[0]))return false;
+        if(mismatch==1 && nums[nums.size()-1]>nums[0])return false;
         return true;
 
 
