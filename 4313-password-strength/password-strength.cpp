2d6c7c34884a6fb4436cd27a830ab30235d7666b
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-26 21:59:36 +0530

    Time: 26 ms (30.62%) | Memory: 17.6 MB (70.44%) - LeetSync

---
+++ 4313-password-strength/password-strength.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int passwordStrength(string password) {
+        int strength=0;
+        set<char>alreadyFound;
+        for(int i=0;i<password.size();i++){
+            if(alreadyFound.find(password[i])!=alreadyFound.end())continue;
+            if(password[i]-'a'>=0 && password[i]-'a'<26)strength++;
+            else if(password[i]-'A'>=0 && password[i]-'A'<26)strength+=2;
+            else if(password[i]-'0'>=0 && password[i]-'0'<10)strength+=3;
+            else strength+=5;
+            alreadyFound.insert(password[i]);
+        }
+        return strength;
+    }
+};
