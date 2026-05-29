a91243e9e207c1754be29d6230def565ed772f6c
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-29 22:03:10 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (49.30%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int mini=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            int num=nums[i];
+            int sum=0;
+            while(num>0){
+                sum+=num%10;
+                num/=10;
+            }
+            mini=min(sum,mini);
+        }
+        return mini;
+    }
+};
