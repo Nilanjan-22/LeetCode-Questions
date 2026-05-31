2fbd211a1cb98232c3e262cb388dfdcd01dbed7b
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-31 18:02:36 +0530

    Time: 214 ms (11.27%) | Memory: 13.9 MB (27.19%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    int ans(int i, int j, vector<int>& nums,vector<vector<int>>& dp){
+    int solve(int i, int j,vector<int>& nums, vector<vector<int>>& dp){ 
         if(i>j)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        int max_coins=INT_MIN;
-        for(int ind=i;ind<=j;ind++){
-            int coins = nums[i-1]*nums[ind]*nums[j+1] + ans(i,ind-1,nums,dp) + ans(ind+1,j,nums,dp);
-            max_coins=max(max_coins,coins);
+        int maxCoins=0;
+        for(int it=i;it<=j;it++){
+            maxCoins=max(maxCoins, nums[i-1]*nums[it]*nums[j+1] + solve(i,it-1,nums,dp)+solve(it+1,j,nums,dp));
         }
-        return dp[i][j] = max_coins;
+        return dp[i][j]=maxCoins;
     }
     int maxCoins(vector<int>& nums) {
-        nums.push_back(1);
-        nums.insert(nums.begin(),1);
-        int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=n-2;i>=1;i--){
-            for(int j=i; j<=n-2 ; j++){
-                int max_coins=INT_MIN;
-                for(int ind=i;ind<=j;ind++){
-                    int coins = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1]+ dp[ind+1][j];
-                    max_coins=max(max_coins,coins);
-                }
-                dp[i][j] = max_coins;
-            }
+        vector<int> numCopy;
+        numCopy.push_back(1);
+        for(auto it: nums){
+            numCopy.push_back(it);
         }
-        return dp[1][n-2];
+        numCopy.push_back(1);
+        int n=numCopy.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(1,n-2,numCopy,dp);
     }
 };
