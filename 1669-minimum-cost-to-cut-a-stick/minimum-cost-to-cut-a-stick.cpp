29c5240dbd6a64b8e88cdc016c4a7781f127de16
Nilanjan-22 <nilanjande9@gmail.com>
2026-05-31 14:19:00 +0530

    Time: 35 ms (45.00%) | Memory: 13.9 MB (78.26%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-//Tabulation solution
-    int ans(int i, int j,int n, vector<int>& cuts,vector<vector<int>>& dp){
-        if(n==1)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int cost=INT_MAX;
-        for(int k=i+1; k<j;k++){
-            if(cuts[k]>cuts[i] && cuts[k]<cuts[j]){
-                int cost_take=n+ans(i,k,cuts[k]-cuts[i],cuts,dp) + ans(k,j,cuts[j]-cuts[k],cuts,dp);
-                cost=min(cost,cost_take);
-            }
+    int ans(int st, int en, vector<int>& cuts , vector<vector<int>>& dp){
+        if(en==st+1)return 0;
+        if(dp[st][en]!=-1)return dp[st][en];
+
+        int minCost=INT_MAX;
+        for(int it=st+1;it<en;it++){
+            minCost=min(minCost, cuts[en]-cuts[st]+ans(st,it,cuts,dp)+ans(it,en,cuts,dp));
         }
-        if(cost==INT_MAX)return dp[i][j] = 0;
-        return dp[i][j] = cost;
+        return dp[st][en]=minCost;
     }
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
-        int s= cuts.size();
+        int s=cuts.size();
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(s,vector<int>(s,0));
-        for(int i=s-1;i>=0;i--){
-            for(int j=i+1;j<s;j++){
-                int cost=INT_MAX;
-                for(int k=i+1; k<j;k++){
-                    if(cuts[k]>cuts[i] && cuts[k]<cuts[j]){
-                        int cost_take=cuts[j]-cuts[i]+dp[i][k]+ dp[k][j];
-                        cost=min(cost,cost_take);
-                    }
-                }
-                if(cost==INT_MAX)dp[i][j] = 0;
-                else dp[i][j] = cost;
-            }
-        }
-        return dp[0][s-1];
-    }
+        vector<vector<int>>dp(s,vector<int>(s,-1));
 
+        return ans(0,s-1,cuts,dp);
+
+    }
 };
