7fa5d4bd4f2281882834c4cf468ff99e19c42947
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-01 22:18:01 +0530

    Time: 1252 ms (47.07%) | Memory: 9.1 MB (99.20%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -21,7 +21,16 @@ public:
     }
     int minCut(string s) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return solve(0,s,dp)-1;
+        vector<int> dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int minCost=INT_MAX;
+            for(int j=i;j<s.size();j++){
+                if(isPalindrome(i,j,s))minCost=min(minCost, 1+dp[j+1]);
+            }
+
+            dp[i]=minCost;
+        }
+        return dp[0]-1;
+        
     }
 };
