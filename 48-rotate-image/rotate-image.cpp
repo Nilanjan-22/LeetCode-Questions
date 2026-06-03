65e409ca7c294fe77edb676de634e38f35cc7c08
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-03 15:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (33.80%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,7 +4,7 @@ public:
         int n=matrix.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(j>=i)swap(matrix[i][j],matrix[j][i]);
+                if(j>i)swap(matrix[i][j],matrix[j][i]);
             }
             reverse(matrix[i].begin(),matrix[i].end());
         }
