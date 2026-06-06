c1ce8fb7c8f25cc7b4abe5f86210b8d2aeeb4dee
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-07 00:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (43.10%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,40 +11,15 @@
  */
 class Solution {
 public:
-    //iterative approach of doing all i.e preorder,postorder and inorder in one go
+void postTraversal(TreeNode* node, vector<int>& postOrder){
+        if(node==NULL)return;
+        postTraversal(node->left, postOrder);
+        postTraversal(node->right , postOrder);
+        postOrder.push_back(node->val);
+    }
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> pre,in,post;
-        stack<pair<TreeNode*,int>> st;
-        if(root==NULL)return post;
-        st.push({root,1});
-        while(!st.empty()){
-            int num=st.top().second;
-            TreeNode* node=st.top().first;
-
-            if(num==1){
-                pre.push_back(node->val);
-                st.pop();
-                st.push({node,2});
-                if(node->left!=NULL){
-                    st.push({node->left,1});
-                }
-            }
-
-            else if(num==2){
-                in.push_back(node->val);
-                st.pop();
-                st.push({node,3});
-                if(node->right!=NULL){
-                    st.push({node->right,1});
-                }
-            }
-
-            else{
-                post.push_back(node->val);
-                st.pop();
-            }
-        }
-
-        return post;
+        vector<int> postOrder;
+        postTraversal(root, postOrder);
+        return postOrder;
     }
 };
