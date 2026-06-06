b13aba232bce2afb1429416e7723103067adc082
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-06 12:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (87.29%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -13,19 +13,21 @@ public:
         return dp[i][j]=totalWays;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        dp[m-1][n-1]=1;
+        vector<int> cur(n+1,0), next(n+1,0);
+        
+        cur[n-1]=1;
 
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(i==m-1 && j==n-1)continue;
                 int totalWays=0;
-                totalWays+=dp[i+1][j];
-                totalWays+=dp[i][j+1];
+                totalWays+=next[j];
+                totalWays+=cur[j+1];
 
-                dp[i][j]=totalWays;
+                cur[j]=totalWays;
             }
+            next=cur;
         }
-        return dp[0][0];
+        return cur[0];
     }
 };
