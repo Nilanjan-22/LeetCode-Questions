5a1fde445ad921430f17729a8f2fee650da2ef53
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-08 17:21:56 +0530

    Time: 349 ms (84.69%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,20 +1,10 @@
 # Write your MySQL query statement below
-with cumulatedAmount as
+with finalTable as
 (
-    select distinct visited_on, sum(amount) over (order by visited_on) as sumAmount, dense_rank() over (order by visited_on) as day_rank
+    select distinct visited_on, sum(amount) over (order by visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) as amount, dense_rank() over (order by visited_on) as day_rank
     from Customer
-),
-
-finalTable as (
-    select a1.visited_on ,(case when a2.visited_on is null then a1.sumAmount else a1.sumAmount-a2.sumAmount end) as amount
-    from
-    cumulatedAmount a1
-    left join 
-    cumulatedAmount a2
-    on
-    a1.day_rank = a2.day_rank+7
-    where a1.day_rank>=7
 )
 
-select * , round((amount/7),2) as average_amount
-from finalTable;
+select visited_on, amount , round((amount/7),2) as average_amount
+from finalTable
+where day_rank>=7
