04c68eb0eed88b747842aed19ffaf919a46b919b
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-11 13:47:11 +0530

    Time: 54 ms (70.81%) | Memory: 24.1 MB (24.89%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,16 +9,40 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        //traverse two linked list at the same time and if anyone of them turns null send it to the head of the other linked list until the intersection is found.
-        ListNode* temp1=headA;
-        ListNode* temp2=headB;
-        while(temp1!=temp2){
-            if(temp1==NULL)temp1=headB;
-            if(temp2==NULL)temp2=headA;
-            if(temp1==temp2)return temp1;
-            temp1=temp1->next;
-            temp2=temp2->next;
+        int size1=0,size2=0;
+        ListNode* it1=headA;
+        ListNode* it2=headB;
+        while(it1!=NULL){
+            it1=it1->next;
+            size1++;
         }
-        return temp1;
+        while(it2!=NULL){
+            it2=it2->next;
+            size2++;
+        }
+        it1=headA;
+        it2=headB;
+        if(size1>size2){
+            int iterations=size1-size2;
+            while(iterations>0){
+                it1=it1->next;
+                iterations--;
+            }
+        }
+        else{
+            int iterations=size2-size1;
+            while(iterations>0){
+                it2=it2->next;
+                iterations--;
+            }
+        }
+
+        while(it1!=NULL){
+            if(it1==it2)return it1;
+            it1=it1->next;
+            it2=it2->next;
+        }
+
+        return NULL;
     }
 };
