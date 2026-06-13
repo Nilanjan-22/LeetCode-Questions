757b08d03d3ec8e9fad5b46ea1934f3d20adc964
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-13 16:33:18 +0530

    Time: 16 ms (8.25%) | Memory: 15 MB (92.23%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,62 +9,43 @@
  */
 class Solution {
 public:
+    TreeNode* lca(TreeNode* root, TreeNode* node1, TreeNode* node2){
+        if(root==NULL || root==node1 || root==node2)return root;
+
+        TreeNode* left=lca(root->left,node1,node2);
+        TreeNode* right=lca(root->right,node1,node2);
+
+        if(!left)return right;
+        else if(!right)return left;
+        else return root;
+    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if(!root)return {};
-        unordered_map<TreeNode*, TreeNode*> parent;
-        vector<int> ans;
-        //bfs traversal
-        queue<TreeNode*>q;  //node
-        int ini;
+        unordered_map<TreeNode*,int> nodeLvl; //stores level of each node
 
-        q.push(root);
-        
-        while(!q.empty()){
-            int s=q.size();
-            for(int i=0;i<s;i++){
-                TreeNode* n=q.front();
-                q.pop();
-                if(n->left){
-                    q.push(n->left);
-                    parent[n->left]=n;
-                }
-                if(n->right){
-                    q.push(n->right);
-                    parent[n->right]=n;
-                }
-            }
-        }
-
-        unordered_map <TreeNode*,bool> vis;
-        int lvl=0;
-        q.push(target);
-        vis[target]=true;
-        while(!q.empty()){
-            int s=q.size();
-            if(lvl==k)break;
-            for(int i=0;i<s;i++){
-                TreeNode* n=q.front();
-                q.pop();
-                if(n->left && !vis[n->left]){
-                    q.push(n->left);
-                    vis[n->left]=true;
-                }
-                if(n->right && !vis[n->right]){
-                    q.push(n->right);
-                    vis[n->right]=true;
-                }
-                if(parent[n] && !vis[parent[n]]){
-                    q.push(parent[n]);
-                    vis[parent[n]]=true;
-                }
-            }
-            lvl++;
-        }
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
 
         while(!q.empty()){
-            ans.push_back(q.front()->val);
+            TreeNode* node=q.front().first;
+            int lvl=q.front().second;
+            nodeLvl[node]=lvl;
             q.pop();
+
+            if(node->left!=NULL)q.push({node->left,lvl+1});
+            if(node->right!=NULL)q.push({node->right,lvl+1});
+
         }
-        return ans;
+        
+        vector<int> nodesAtKDistance;
+        for(auto it: nodeLvl){
+            TreeNode* node2=it.first;
+            TreeNode* ancestor = lca(root,target,node2);
+            int distance = nodeLvl[target]+nodeLvl[node2]-2*nodeLvl[ancestor];
+            if(distance==k){
+                nodesAtKDistance.push_back(node2->val);
+            }
+        }
+
+        return nodesAtKDistance;
     }
 };
