5c5f30b4aad6fd48931134b424bd83cf69b5320f
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-14 13:55:21 +0530

    Time: 143 ms (5.32%) | Memory: 29.6 MB (6.43%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        stack<int> st;
+        vector<int> nge(n),pge(n);
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&& height[st.top()]<=height[i]){
+                st.pop();
+            }
+            if(st.empty())nge[i]=-1;
+            else nge[i]=st.top();
+            st.push(i);
+        }
+
+        st={};
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && height[st.top()]<=height[i]){
+                st.pop();
+            }
+            if(st.empty())pge[i]=-1;
+            else pge[i]=st.top();
+            st.push(i);
+        }
+
+        int totalWater=0;
+        map<int,int> alreadyConsidered;
+        for(int i=0;i<n;i++){
+            if(nge[i]==-1 || pge[i]==-1)continue;
+            else {
+                if(alreadyConsidered.find(height[i])!=alreadyConsidered.end() && alreadyConsidered[height[i]]>i)continue;
+                int h=min(height[nge[i]],height[pge[i]])-height[i];
+                int w=nge[i]-pge[i]-1;
+                alreadyConsidered[height[i]]=nge[i];
+                cout<<"index : "<<i<<" "<<"value : "<<w*h<<endl;
+                totalWater+=w*h;
+            }
+        }
+        return totalWater;
+        
+    }
+};
