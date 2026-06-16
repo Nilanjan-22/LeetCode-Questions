9116c75651035c8380a687cb4bac9bde38b2a881
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-17 03:56:48 +0530

    Time: 54 ms (14.36%) | Memory: 8.6 MB (74.65%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s="";
+        for(int i=1;i<=n;i++)s+=(to_string(i));
+        int t=k;
+        while(t>1){
+            next_permutation(s.begin(),s.end());
+            t--;
+        }
+        return s;
+    }
+};
