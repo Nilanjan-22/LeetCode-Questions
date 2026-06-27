b7c3c71644f376ee11235a89f7135acd03dcae43
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-27 12:38:18 +0530

    Time: 3 ms (27.41%) | Memory: 16.8 MB (52.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>> directions = {{-1,0},{1,0},{0,1},{0,-1}};
-        int freshOranges=0;
-        queue<vector<int>> q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2)q.push({i,j,0});
-                if(grid[i][j]==1)freshOranges++;
+        int n = grid.size(), m = grid[0].size();
+        int freshOranges = 0;
+        
+        // Use a pair instead of a vector to avoid heap allocation overhead
+        queue<pair<int, int>> q;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 2) q.push({i, j});
+                else if(grid[i][j] == 1) freshOranges++;
             }
         }
-        int maxTime=0;
+        
+        // Early exit if there are no fresh oranges to begin with
+        if(freshOranges == 0) return 0;
+        
+        int maxTime = 0;
+        // Use a basic C-array for fixed directions
+        int directions[4][2] = {{-1,0}, {1,0}, {0,1}, {0,-1}};
+        
         while(!q.empty()){
-            int i=q.front()[0],j=q.front()[1],time=q.front()[2];
-            maxTime=max(time,maxTime);
-            q.pop();
-
-            for(auto dir:directions){
-                int ni=i+dir[0],nj=j+dir[1];
-
-                if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1){
-                    freshOranges--;
-                    grid[ni][nj]=2;
-                    q.push({ni,nj,time+1});
+            int size = q.size();
+            bool rottedThisMinute = false;
+            
+            // Process the queue level-by-level (minute-by-minute)
+            while(size--){
+                auto [i, j] = q.front();
+                q.pop();
+                
+                for(auto& dir : directions){
+                    int ni = i + dir[0];
+                    int nj = j + dir[1];
+                    
+                    if(ni >= 0 && ni < n && nj >= 0 && nj < m && grid[ni][nj] == 1){
+                        grid[ni][nj] = 2;
+                        freshOranges--;
+                        q.push({ni, nj});
+                        rottedThisMinute = true; // Track if we actually rotted anything
+                    }
                 }
             }
+            // Only increment time if we successfully rotted adjacent oranges this round
+            if(rottedThisMinute) maxTime++; 
         }
-        if(freshOranges>0)return -1;
-        return maxTime;
+        
+        return freshOranges == 0 ? maxTime : -1;
     }
 };
