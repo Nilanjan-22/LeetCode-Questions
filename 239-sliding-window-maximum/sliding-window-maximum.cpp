44610f2998522ace8c02d8da7cf0e9d6e4b0db08
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-27 10:51:54 +0530

    Time: 315 ms (8.17%) | Memory: 175.4 MB (7.56%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,42 +2,17 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int> ans, nge(n),pge(n);
-
-        stack<int> st;
-
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[st.top()]<=nums[i]){
-                st.pop();
-            }
-
-            if(st.empty())nge[i]=n;
-            else nge[i]=st.top();
-
-            st.push(i);
+        vector<int> ans(n-k+1);
+        map<int,int> mp;
+        for(int i=0;i<k;i++){
+            mp[nums[i]]++;
         }
-
-        st={};
-
-        for(int i=0;i<n;i++){
-            while(!st.empty() && nums[st.top()]<nums[i]){
-                st.pop();
-            }
-
-            if(st.empty())pge[i]=-1;
-            else pge[i]=st.top();
-
-            st.push(i);
-        }
-
-        for(int i=0;i<n;i++){
-            int r=min(k,i-pge[i]);
-            int l=min(k,nge[i]-i);
-            if(r+l-1<k)continue;
-            int times=(r+l-1)%k;
-            for(int it=0;it<=times;it++){
-                ans.push_back(nums[i]);
-            }
+        for(int i=0;i<n-k+1;i++){
+            int maxNum=mp.rbegin()->first;
+            ans[i]=maxNum;
+            mp[nums[i]]--;
+            if(mp[nums[i]]==0)mp.erase(nums[i]);
+            if(i+k<n)mp[nums[i+k]]++;
         }
         return ans;
     }
