8733128241da954121780c58109fc5924610e83f
Nilanjan-22 <nilanjande9@gmail.com>
2026-06-29 17:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (33.74%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int numOfStrings(vector<string>& patterns, string word) {
-        int n=word.size();
-        unordered_map<string,int> patternsFreq;
-        for(auto s: patterns){
-            patternsFreq[s]++;
-        }
-
-        for(int i=0;i<n;i++){
-            string str="";
-            for(int j=i;j<n;j++){
-                str+=word[j];
-                if(patternsFreq.find(str)!=patternsFreq.end()){
-                    patternsFreq[str]=0;
-                }
+    int numOfStrings(vector<string>& patterns, string word) 
+    {
+        int cnt=0;
+        for(string p:patterns)
+        {
+            if(word.find(p)!=string::npos)
+            {
+                cnt++;
             }
-        }
 
-        int ans=patterns.size();
-        for(auto it: patternsFreq){
-            ans-=it.second;
         }
-        return ans;
-
+        return cnt;
     }
+
 };
