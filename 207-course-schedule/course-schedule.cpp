adbee89cadf9a66fc624329c428c61debfbda4c0
Nilanjan-22 <nilanjande9@gmail.com>
2026-07-17 14:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (55.13%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int N=numCourses;
-        vector<vector<int>> adj(N);
-        vector<int> indeg(N);
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][1];
-            int v=prerequisites[i][0];
-            indeg[v]++;
-            adj[u].push_back(v);
-        }
-        
-        vector<int> toposort;
-        queue<int> q;
-        for(int i=0;i<N;i++){
-            if(indeg[i]==0)q.push(i);
-        }
-        while(!q.empty()){
-            int u=q.front();
-            toposort.push_back(u);
-            q.pop();
-            
-            for(auto it: adj[u]){
-                indeg[it]--;
-                if(indeg[it]==0)q.push(it);
+    bool loopFoundDFS(int node, vector<vector<int>>& adj, vector<int>& path, vector<int>& vis){
+        vis[node]=1;
+        path[node]=1;
+        bool foundLoop=false;
+        for(auto it: adj[node]){
+            if(path[it]==1)return true;
+            else if(vis[it]!=1){
+                foundLoop|=loopFoundDFS(it,adj,path,vis);
             }
         }
-        return toposort.size()==N;
+        path[node]=0;
+        return foundLoop;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            int u = prerequisites[i][1], v = prerequisites[i][0];
+            adj[u].push_back(v);
+        }
+
+        vector<int> path(numCourses,0);
+        vector<int> vis(numCourses,0);
+        bool loopFound=false;
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){
+                loopFound|=loopFoundDFS(i,adj,path,vis);
+                if(loopFound==true)return false;
+            }
+        }
+        return true;
     }
 };
