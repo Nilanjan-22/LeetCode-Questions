e04ddcaed7955e68294be9f84f992cd169293811
Nilanjan-22 <nilanjande9@gmail.com>
2026-07-22 16:31:54 +0530

    Time: 46 ms (14.97%) | Memory: 27.7 MB (16.44%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int solve(int pos1, int pos2, string& s1, string& s2, vector<vector<int>>& dp){
-        if(pos1>=s1.size() || pos2>=s2.size())return 0;
+    int LCS(int i ,  int j, string& s1, string& s2, vector<vector<int>>& dp){
+        if(i>=s1.size() || j>=s2.size())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
 
-        if(dp[pos1][pos2]!=-1)return dp[pos1][pos2];
-
-        if(s1[pos1]==s2[pos2])return dp[pos1][pos2]=1+solve(pos1+1,pos2+1,s1,s2,dp);
-
-        int maxLength=0;
-
-        maxLength=max(maxLength, solve(pos1+1,pos2,s1,s2,dp));
-        maxLength=max(maxLength, solve(pos1,pos2+1,s1,s2,dp));
-
-
-        return dp[pos1][pos2]=maxLength;
+        int maxLen=0;
+        if(s1[i]==s2[j])maxLen = max(maxLen, 1+LCS(i+1,j+1,s1,s2,dp));
+        else{
+            maxLen=max(maxLen , LCS(i+1,j,s1,s2,dp));
+            maxLen=max(maxLen, LCS(i,j+1,s1,s2,dp));
+        }
+        return dp[i][j]=maxLen;
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size(),m=text2.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-
-        return solve(0,0,text1,text2,dp);
+        return LCS(0,0,text1,text2,dp);
     }
 };
