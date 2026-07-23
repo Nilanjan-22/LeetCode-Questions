8576ed517ebaf5ae05d004827babf9bd2d458ca3
Nilanjan-22 <nilanjande9@gmail.com>
2026-07-24 04:14:34 +0530

    Time: 7 ms (60.78%) | Memory: 13.2 MB (60.36%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int solve(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
-        if(i<0){
-            if(j<0)return 0;
-            return j+1;
-        }
-        if(j<0)return i+1;
+    int solve(int i,int j, string& w1, string& w2, vector<vector<int>>& dp){
+        if(i>=w1.size())return w2.size()-j;
+        if(j>=w2.size())return w1.size()-i;
 
         if(dp[i][j]!=-1)return dp[i][j];
 
-        int minOps=INT_MAX;
-        //character matched
-        if(word1[i]==word2[j])return dp[i][j]=solve(i-1,j-1,word1,word2,dp);
-
-        //insert
-        minOps=min(minOps,1+solve(i,j-1,word1,word2,dp));
-
-        //delete
-        minOps=min(minOps, 1+solve(i-1,j,word1,word2,dp));
-
-        //replace
-        minOps=min(minOps,1+solve(i-1,j-1,word1,word2,dp));
-
-        return dp[i][j]=minOps;
-
+        int minSteps=0;
+        if(w1[i]==w2[j]){
+            return dp[i][j]=solve(i+1,j+1,w1,w2,dp);
+        }
+        else{
+            minSteps= 1+min(solve(i+1,j,w1,w2,dp),min(solve(i,j+1,w1,w2,dp), solve(i+1,j+1,w1,w2,dp)));
+        }
+        return dp[i][j]=minSteps;
     }
     int minDistance(string word1, string word2) {
-        int n=word1.size(),m=word2.size();
+        int n=word1.size(), m=word2.size();
+        if(n==0)return m;
+        if(m==0)return n;
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,word1,word2,dp);
-    }  
+        return solve(0,0,word1,word2,dp);
+    }
 };
