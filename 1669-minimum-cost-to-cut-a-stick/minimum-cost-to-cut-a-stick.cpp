0aa2f6fb3f2e3f8232ffb366878ac420865142e1
Nilanjan-22 <nilanjande9@gmail.com>
2026-07-25 04:09:30 +0530

    Time: 42 ms (10.79%) | Memory: 15.3 MB (5.04%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    int ans(int st, int en, vector<int>& cuts , vector<vector<int>>& dp){
-        if(en==st+1)return 0;
+    long long solve(int st, int en, vector<int>& cuts, vector<vector<long long>>& dp){
+        if(en<=st+1)return 0;
         if(dp[st][en]!=-1)return dp[st][en];
 
-        int minCost=INT_MAX;
-        for(int it=st+1;it<en;it++){
-            minCost=min(minCost, cuts[en]-cuts[st]+ans(st,it,cuts,dp)+ans(it,en,cuts,dp));
+        long long minCost=INT_MAX;
+
+        for(int i=st+1;i<en;i++){
+            minCost = min(minCost , cuts[en]-cuts[st] +solve(st,i,cuts,dp)+solve(i,en,cuts,dp));
         }
         return dp[st][en]=minCost;
     }
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
-        int s=cuts.size();
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>>dp(s,vector<int>(s,0));
+        int s=cuts.size()-1;
 
-        for(int st=s-1; st>=0; st--){
-            for(int en=st+1; en<s; en++){
-                int minCost=INT_MAX;
-                for(int it=st+1;it<en;it++){
-                    minCost=min(minCost, cuts[en]-cuts[st]+dp[st][it]+dp[it][en]);
-                }
-                if(minCost<INT_MAX)dp[st][en]=minCost;
-            }
-        }
-        return dp[0][s-1];
-
+        vector<vector<long long>> dp(s+1,vector<long long>(s+1,-1));
+        return solve(0,s,cuts,dp);
     }
 };
