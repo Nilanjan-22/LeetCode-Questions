81030cf417e69e4476dfd9bdec57feb023ba5fd8
Nilanjan-22 <nilanjande9@gmail.com>
2026-07-26 13:10:15 +0530

    Time: 726 ms (8.71%) | Memory: 126.3 MB (5.02%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int maxProfit(int i, vector<vector<int>>& jobs, vector<int>& dp){
+    int solve(int i, vector<vector<int>>& jobs, vector<int>& dp){
         if(i>=jobs.size())return 0;
         if(dp[i]!=-1)return dp[i];
 
-        int ntake=maxProfit(i+1,jobs,dp);
-        vector<int> search = {jobs[i][1],0,0};
-        int nextInd= lower_bound(jobs.begin(),jobs.end(),search) - jobs.begin();
-        int take=jobs[i][2]+maxProfit(nextInd,jobs,dp);
+        int maxProfit=0;
+        //take
+        int nextInd = lower_bound(jobs.begin(),jobs.end(),vector<int>{jobs[i][1],0,0}) - jobs.begin();
+        maxProfit = max(maxProfit, jobs[i][2]+solve(nextInd,jobs,dp));
+        //notTake
+        maxProfit = max(maxProfit, solve(i+1,jobs,dp));
 
-        return dp[i]=max(ntake,take);
+        return dp[i]=maxProfit;
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n=profit.size();
         vector<vector<int>> jobs;
+        int n=profit.size();
         for(int i=0;i<n;i++){
-            jobs.push_back({startTime[i],endTime[i],profit[i]});
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
         }
         sort(jobs.begin(),jobs.end());
         vector<int> dp(n,-1);
-
-        return maxProfit(0,jobs,dp);
+        return solve(0,jobs,dp);
     }
 };
