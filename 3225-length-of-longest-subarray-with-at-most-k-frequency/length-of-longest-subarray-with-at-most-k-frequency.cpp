2a5df67824b5ef6c1e852ac32c84181e2fe650ac
Nilanjan-22 <nilanjande9@gmail.com>
2026-08-12 22:31:14 +0530

    Time: 63 ms (63.26%) | Memory: 149.3 MB (84.27%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        int st=0, en=0;
-        map<int, set<int>> hashMap;
-        map<int,int> freq;
+        int st=0,en=0;
         int maxSubarrayLen=-1;
+        unordered_map<int,int> hash;
         while(en<nums.size()){
-            int prevFreq = 0;
-            if(freq.find(nums[en])!=freq.end()){
-                prevFreq = freq[nums[en]];   
-                hashMap[prevFreq].erase(nums[en]);
+            hash[nums[en]]++;
+            while(hash[nums[en]]>k){
+                hash[nums[st]]--;
+                st++;
             }
-            if(hashMap[prevFreq].empty())hashMap.erase(prevFreq);
-
-            freq[nums[en]]++;
-            hashMap[prevFreq+1].insert(nums[en]);
-
-            int maxFreq = hashMap.rbegin()->first;
-            if(maxFreq<=k){
-                maxSubarrayLen = max(maxSubarrayLen, en-st+1);
-                en++;
-            }
-            else{
-                while(maxFreq>k){
-                    int curFreq = freq[nums[st]];
-                    if(freq[nums[st]]==1){
-                        freq.erase(nums[st]);
-                    }
-                    else freq[nums[st]]--;
-                
-                    if(hashMap[curFreq].size()==1)hashMap.erase(curFreq);
-                    else hashMap[curFreq].erase(nums[st]);
-
-                    if(curFreq!=1){
-                        hashMap[curFreq-1].insert(nums[st]);
-                    }
-                    st++;
-                    maxFreq=hashMap.rbegin()->first;;
-                }
-                en++;
-            }
+            maxSubarrayLen=max(maxSubarrayLen, en-st+1);
+            en++;
         }
         return maxSubarrayLen;
     }
