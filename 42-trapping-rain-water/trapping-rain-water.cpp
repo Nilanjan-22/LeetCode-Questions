72a5b8621c9792f9b43d0bc7e7c3654d8ebb9d79
Nilanjan-22 <nilanjande9@gmail.com>
2026-08-13 20:59:39 +0530

    Time: 2 ms (18.90%) | Memory: 26.7 MB (40.24%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,42 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        stack<int> st;
-        vector<int> nge(n),pge(n);
-
+        int trappedWater = 0;
+        vector<int> towers(n);
+        int maxHeight=0;
+        for(int i=0;i<n;i++){
+            towers[i]=maxHeight;
+            maxHeight = max(maxHeight,height[i]);
+        }
+        maxHeight=0;
         for(int i=n-1;i>=0;i--){
-            while(!st.empty()&& height[st.top()]<=height[i]){
-                st.pop();
-            }
-            if(st.empty())nge[i]=-1;
-            else nge[i]=st.top();
-            st.push(i);
+            towers[i]=min(maxHeight,towers[i]);
+            trappedWater += max(0,towers[i]-height[i]);
+            maxHeight = max(maxHeight,height[i]);
         }
 
-        st={};
+        return trappedWater;
 
-        for(int i=0;i<n;i++){
-            while(!st.empty() && height[st.top()]<=height[i]){
-                st.pop();
-            }
-            if(st.empty())pge[i]=-1;
-            else pge[i]=st.top();
-            st.push(i);
-        }
-
-        int totalWater=0;
-        unordered_map<int,int> alreadyConsidered;
-        for(int i=0;i<n;i++){
-            if(nge[i]==-1 || pge[i]==-1)continue;
-            else {
-                if(alreadyConsidered.find(height[i])!=alreadyConsidered.end() && alreadyConsidered[height[i]]>i)continue;
-                int h=min(height[nge[i]],height[pge[i]])-height[i];
-                int w=nge[i]-pge[i]-1;
-                alreadyConsidered[height[i]]=nge[i];
-                totalWater+=w*h;
-            }
-        }
-        return totalWater;
-        
     }
 };
